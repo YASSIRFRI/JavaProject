9d18992a56901a266a6a3016b1afe7de9f346b63
Yassir Fri <yassir.fri@um6p.ma>
2022-12-02 00:06:29 +0100

    All chess pieces in place

--- ChessGame/src/GameBoard.java
+++ ChessGame/src/GameBoard.java
@@ -41,25 +41,35 @@ class ChessBoard extends GameBoard {
     @Override
     public void fillBoard() {
         for (int i = 0; i < 8; i++) {
+            if(i%2==0)
+            {
             board[i][1].setPlaceholder(new Pawn(true));
             board[i][6].setPlaceholder(new Pawn(false));
+            }
+            else
+            {
+                board[i][1].setPlaceholder(new Pawn(false));
+                board[i][6].setPlaceholder(new Pawn(true));
+            }
         }
-        board[7][0].setPlaceholder(new Rook(true));
         board[0][0].setPlaceholder(new Rook(false));
-        board[0][7].setPlaceholder(new Rook(false));
+        board[0][7].setPlaceholder(new Rook(true));
         board[7][7].setPlaceholder(new Rook(false));
         board[1][0].setPlaceholder(new Knight(true));
-        board[6][0].setPlaceholder(new Knight(true));
+        board[6][0].setPlaceholder(new Knight(false));
         board[1][7].setPlaceholder(new Knight(false));
-        board[6][7].setPlaceholder(new Knight(false));
+        board[6][7].setPlaceholder(new Knight(true));
         board[2][0].setPlaceholder(new Bishop(false));
         board[5][0].setPlaceholder(new Bishop(true));
         board[2][7].setPlaceholder(new Bishop(true));
         board[5][7].setPlaceholder(new Bishop(false));
         board[3][0].setPlaceholder(new Queen(true));
-        board[4][0].setPlaceholder(new King(true));
-        board[7][7].setPlaceholder(new Queen(false));
-        board[4][7].setPlaceholder(new King(false));
+        board[3][7].setPlaceholder(new Queen(false));
+        board[4][0].setPlaceholder(new King(false));
+        board[7][7].setPlaceholder(new Rook(false));
+        board[7][0].setPlaceholder(new Rook(true));
+
+        board[4][7].setPlaceholder(new King(true));
         for(int i=0;i<8;i++)
         {
             for(int j=0;j<8;j++)
