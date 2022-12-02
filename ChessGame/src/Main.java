bf1a056c1f989b3d2abf2f2f43451c4bc4135da1
zineb abercha <ZINEB.ABERCHA@um6p.ma>
2022-12-02 16:44:22 +0100

    removed other game

--- ChessGame/src/Main.java
+++ ChessGame/src/Main.java
@@ -34,10 +34,6 @@ public class Main extends Application {
     primaryStage.setScene(scene);
     primaryStage.show();
   }
-  public static void main(String[] args) {
-    launch(args);
-  }
-
-  
+ 
 }
  
